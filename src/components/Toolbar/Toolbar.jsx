464109b4cebfbf923c3b994c4fa5a2ed1a9c3428
User <User@DESKTOP-C51DDIB>
2023-05-17 17:37:11 +0500

    first commit

---
+++ src/components/Toolbar/Toolbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Toolbar.css';
+
+const Toolbar = () => {
+  return (
+    <div>
+        <nav>
+        <ul>
+            <li>
+                Training list
+            </li>
+            <li>
+                About
+            </li>
+        </ul>
+
+        </nav>
+    </div>
+  )
+}
+
+export default Toolbar;
